349417f9ec8b213154a51d722dfae7f2e7cde9ee
Vinayak Ambigapathy <vinayak.ambigapathy@kainoo.ch>
2025-04-24 18:55:42 +0200

    Initial commit

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,6 +6,7 @@ import CodeViewer from '@/Components/CodeViewer.vue';
 
 const page = ref("");
 const mode = ref(false);
+const edit = ref(true);
 </script>
 
 <template>
@@ -15,9 +16,12 @@ const mode = ref(false);
             <button 
             @click="mode = !mode"
             class="bg-gray-200 text-gray-700 px-2 w-fit rounded shadow">{{mode ? "Raw" : "Normal" }}</button>
+            <button 
+            @click="edit = !edit"
+            class="bg-gray-200 text-gray-700 px-2 w-fit rounded shadow ml-3">{{edit ? "Close" : "Edit" }}</button>
         </div>
-        <div class="grid grid-cols-2">
-            <Editor @update="(data) => page = data.html" class="border-r border-gray-100"/>
+        <div class="grid" :class="edit ? 'grid-cols-2' : 'px-96'">
+            <Editor v-show="edit" @update="(data) => page = data.html" class="border-r border-gray-100"/>
             <CodeViewer v-if="!mode" :code="page"/>
             <div v-else class="tiptap-editor" v-html="page">
             </div>
